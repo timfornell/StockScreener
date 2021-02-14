40736f2941ea413b5709cc1206ea227a058dac00
Tim Fornell <timfo734@student.liu.se>
2021-02-14 10:53:48 +0100

    Finish scrollbar and add sorting by clicking on columns

--- Code/GUI.py
+++ Code/GUI.py
@@ -34,7 +34,7 @@ class GUI(Frame):
 
         # Sort options
         self.sort_frame = Frame(self.options_frame)
-        self.sort_frame.pack(side=TOP)
+        self.sort_frame.pack(side=LEFT)
         self.sort_var = StringVar(self.sort_frame)
         self.sort_label = Label(self.sort_frame, text="Sorting key:")
         self.sort_key = OptionMenu(self.sort_frame, self.sort_var, *self.sort_options, command=self.set_sort_variable)
@@ -45,7 +45,7 @@ class GUI(Frame):
 
         # Number of stocks to display
         self.num_stocks_frame = Frame(self.options_frame)
-        self.num_stocks_frame.pack(side=BOTTOM)
+        self.num_stocks_frame.pack(side=RIGHT)
         self.number_of_stocks_label = Label(self.num_stocks_frame, text="Number of stocks to show:")
         self.number_of_stocks_entry = Entry(self.num_stocks_frame)
         self.number_of_stocks_entry.pack(side=LEFT)
@@ -57,23 +57,43 @@ class GUI(Frame):
 
         # Setup treeview
         self.tree_frame = Frame(self.root)
-        self.tree_frame.pack(side=BOTTOM)
+        self.tree_frame.pack(side=BOTTOM, fill="both", expand=True)
         self.tree = ttk.Treeview(self.tree_frame, columns=tuple(self.stocklist.columns), selectmode='browse')
-        self.tree.pack(side=LEFT, fill="x")
+        self.tree.bind("<Button-1>", self.mouse_click)
+        self.tree.pack(side=RIGHT, fill="both", expand=True)
     
         for i, col in enumerate(self.stocklist.columns):
             self.tree.heading("{}".format(i), text=col)
-            self.tree.column("{}".format(i), stretch=YES)
+            self.tree.column("{}".format(i), stretch=YES, width=120)
 
         self.tree["show"] = "headings"
         self.treeview = self.tree
 
-        self.vert_scrollbar = Scrollbar(self.tree_frame, orient="vertical", command=self.tree.yview)
-        self.vert_scrollbar.pack(side=RIGHT, fill="y")
-
+        self.scroll_frame = Frame(self.tree_frame)
+        self.scroll_frame.pack(side=LEFT, fill="y")
+        self.vert_scrollbar = Scrollbar(self.scroll_frame, orient="vertical", command=self.tree.yview)
+        self.vert_scrollbar.pack(side=LEFT, fill="y", anchor=W)
         self.tree.configure(yscrollcommand=self.vert_scrollbar.set)
 
+        # Bind keys to functions
+        self.root.bind("<Return>", self.enter_key_callback)
+
         self.update_stocklist(initialize=True)
+
+    
+    def mouse_click(self, event):
+        region = self.tree.identify("region", event.x, event.y)
+        column = self.tree.identify_column(event.x)
+        heading = self.tree.heading(column)
+        self.sort_variable_string = heading["text"]
+        self.sort_var.set(heading["text"])
+        self.sort_stocklist()
+
+
+    def enter_key_callback(self, event):
+        focused_widget = self.root.focus_get()
+        if focused_widget is self.number_of_stocks_entry:
+            self.update_stocklist()
 
 
     def set_sort_variable(self, selection) -> None:
@@ -107,7 +127,7 @@ class GUI(Frame):
 
     def insert_data(self, id, stock) -> None:
         # For some reason 'text=' corresponds to the first column in the tree
-        self.treeview.insert("", "end", iid=id, text=stock[1][0], values=tuple(stock[1][1::]))
+        self.treeview.insert("", "end", iid=id, values=tuple(stock[1]))
 
     
     def clear_tree(self) -> None:
